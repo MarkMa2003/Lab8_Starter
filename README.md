861206393addd127ab51bde5ed6e40726678ed25
MarkMa2003 <ke.mark.ma2021@gmail.com>
2023-05-27 17:29:43 -0700

    u

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Lab 8 - Starter
+1. I will choose 2, since we can munually run the code very easily with go live functionality in the VScode, whereas the other two will takes more time, and not very intuitive to debug. 
+
+2. No, we can just check the output instead of emulate user actions.
+
+3. No, since the message feature is better to tested with emulating user actions, instead of just check the part.
+
+4. Yes, because their is a definite result and clear scope of what we want, so we can use unit test.
+
+![p1](p1.png)
+![p2](p2.png)
+![p3](p3.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,67 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('correct phone number',()=>{
+    expect(functions.isPhoneNumber('133-223-4334')).toBe(true);
+})
+test('correct phone number',()=>{
+    expect(functions.isPhoneNumber('124-333-3333')).toBe(true);
+})
+test('incorrect phone number',()=>{
+    expect(functions.isPhoneNumber('133-223-433')).toBe(false);
+})
+test('incorrect phone number',()=>{
+    expect(functions.isPhoneNumber('1243333')).toBe(false);
+})
+
+test('correct email',()=>{
+    expect(functions.isEmail('hello@hello.com')).toBe(true);
+})
+test('correct email',()=>{
+    expect(functions.isEmail('hello@gmail.com')).toBe(true);
+})
+test('incorrect email',()=>{
+    expect(functions.isEmail('hello.com')).toBe(false);
+})
+test('incorrect email',()=>{
+    expect(functions.isEmail('ffffcom')).toBe(false);
+})
+
+test('valid strong password',()=>{
+    expect(functions.isStrongPassword('hello123')).toBe(true);
+})
+test('valid strong password',()=>{
+    expect(functions.isStrongPassword('qwertasd')).toBe(true);
+})
+test('invalid strong password',()=>{
+    expect(functions.isStrongPassword('123')).toBe(false);
+})
+test('valid strong password',()=>{
+    expect(functions.isStrongPassword('0')).toBe(false);
+})
+
+test('correct date',()=>{
+    expect(functions.isDate('22/22/2222')).toBe(true);
+})
+test('correct date',()=>{
+    expect(functions.isDate('11/11/1111')).toBe(true);
+})
+test('incorrect date',()=>{
+    expect(functions.isDate('123')).toBe(false);
+})
+test('incorrect date',()=>{
+    expect(functions.isDate('2021')).toBe(false);
+})
+
+test('correct color',()=>{
+    expect(functions.isHexColor('#123456')).toBe(true);
+})
+test('correct color',()=>{
+    expect(functions.isHexColor('#123')).toBe(true);
+})
+test('incorrect color',()=>{
+    expect(functions.isHexColor('3456')).toBe(false);
+})
+test('incorrect color',()=>{
+    expect(functions.isHexColor('#1223')).toBe(false);
+})
